e6b1d349effc60f1b768c22ae701459edb726b76
Nick <nicholasan98@gmail.com>
2022-11-30 02:59:01 -0500

    Update minesweeper.py

--- Minesweeper/minesweeper.py
+++ Minesweeper/minesweeper.py
@@ -1,9 +1,43 @@
 # lets create a board object to represent the minesweeper game
 # this is so that we can just say "create a new board object", or
 # "dig here", or "render this game for this object"
+import random
 
 class Board:
     def __init__(self, dim_size, num_bombs):
+        # let's keep track of these parameters. they'll be helpful later
+        self.dim_size = dim_size
+        self.num_bombs = num_bombs
+
+        # let's create the board
+        self.board = self.make_new_board() # plant the bombs
+
+        # initialize a set to keep track of which locations we''ve uncovered
+        # we'll save (row, col) as tuples into this set
+        self.dug = set() # if we dig at (0, 0) then self.dug = {(0, 0)}
+
+    def make_new_board(self):
+        # construct a new board based on the dim size and num bombs
+        # we should construct the list of lists here (or whatever representation you prefer,
+        # but since we have a 2-D board, list of lists is most natural)
+
+        # generate a new board
+        board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)]
+        # this creates an array like this:
+        # [ [None, None, ..., None],
+        #   [None, None, ..., None],
+        #   [...,  ..., ...,  ...],
+        #   [None, None, ..., None]]
+        # we can see how this represents the board
+
+        # plant the bombs
+        bombs_planted = 0
+        while bombs_planted < self.num_bombs:
+            loc = random.randint(0, self.dim_size**2 - 1) # return a random integer N such that a <= N <= b
+            row = loc // self.dim_size  # we want the number of times dim_size goes into loc to tell us what row to look at
+            col = loc % self.dim_size # we want the remainder to tell us what index in that row to look at
+
+            
 
 # play the game
 def play(dim_size = 10, num_bombs = 10):
